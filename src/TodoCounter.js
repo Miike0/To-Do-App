1321dd195c6638fc047ada2417f340913422bac2
Miikeo <Miguel.A.INCO@gmail.com>
2022-12-08 20:42:48 -0600

    Created to do components; TodoCounter, TodoSearch, TodoList, TodoItem, CreateTodoButton

---
+++ src/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TodoCounter() {
+    return (
+        <h2>2 of 3 TODOs completed</h2>
+
+    );
+}
+
+export { TodoCounter };
---
+++ src/TodoSearch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TodoSearch() {
+    return (
+        <input placeholder="Type something to do"/>
+
+    );
+}
+
+export { TodoSearch };
