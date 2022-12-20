7e8538180683a54ac9a8b69ebaa92dad7e269fd2
Miikeo <Miguel.A.INCO@gmail.com>
2022-12-20 11:13:52 -0600

    Local storage used to save changes even though reload the page or close the browser

--- src/App/index.js
+++ src/App/index.js
@@ -2,23 +2,35 @@ import React from "react";
 import './App.css';
 import { AppUI } from "./AppUI";
 
-const defaultToDos = [
-    { text: 'Prueba 1', completed: false},
-    { text: 'Prueba 2', completed: false},
-    { text: 'Prueba 3', completed: true},
-    { text: 'Prueba 4', completed: false},
-    { text: 'Prueba 5', completed: false},
-    { text: 'Prueba 6', completed: true},
-    { text: 'Prueba 7', completed: false},
-    { text: 'Prueba 8', completed: false},
-    { text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', completed: true},
+// const defaultToDos = [
+//     { text: 'Prueba 1', completed: false},
+//     { text: 'Prueba 2', completed: false},
+//     { text: 'Prueba 3', completed: true},
+//     { text: 'Prueba 4', completed: false},
+//     { text: 'Prueba 5', completed: false},
+//     { text: 'Prueba 6', completed: true},
+//     { text: 'Prueba 7', completed: false},
+//     { text: 'Prueba 8', completed: false},
+//     { text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', completed: true},
 
 
-];
+// ];
 
 function App() {
 
-  const [toDos, setToDos] = React.useState(defaultToDos);
+  const localStorageToDos = localStorage.getItem('TODOS_V1');
+  
+  let parsedToDos;
+
+  if(!localStorageToDos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedToDos = [];
+
+  } else {
+    parsedToDos = JSON.parse(localStorageToDos);
+  }
+
+  const [toDos, setToDos] = React.useState(parsedToDos);
 
   const [searchValue, setSearchValue] = React.useState('');
 
@@ -49,7 +61,7 @@ function App() {
 
     newTodDos[todoIndex].completed = true;
 
-    setToDos(newTodDos);
+    saveChangesInLS(newTodDos);
   };
 
   const deleteToDo = (text) => {
@@ -57,11 +69,18 @@ function App() {
         todo => todo.text === text
     );
     
-    const newTodDos = [...toDos];
+    const newToDos = [...toDos];
 
-    newTodDos.splice(todoIndex, 1);
+    newToDos.splice(todoIndex, 1);
 
-    setToDos(newTodDos);
+    saveChangesInLS(newToDos);
+  };
+
+  const saveChangesInLS = (saveChanges) => {
+    const stringifyToDos = JSON.stringify(saveChanges);
+    localStorage.setItem('TODOS_V1', stringifyToDos);
+
+    setToDos(saveChanges);
   };
 
   
